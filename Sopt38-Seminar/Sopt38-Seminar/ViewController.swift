103467e41c4a7b3aac2cf6159b66904a2c6e1252
yeonee <leenayeon@sookmyung.ac.kr>
2026-02-19 16:34:25 +0900

    setting: 프로젝트 생성

---
+++ Sopt38-Seminar/Sopt38-Seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sopt38-Seminar
+//
+//  Created by 이나연 on 2/19/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
