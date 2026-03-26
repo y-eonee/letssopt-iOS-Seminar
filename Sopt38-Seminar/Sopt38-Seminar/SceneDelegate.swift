7990214f09a0f0be11d4d148d68d6104ef36d474
yeonee <leenayeon@sookmyung.ac.kr>
2026-03-26 19:08:09 +0900

    [3주차] 폴더링

--- Sopt38-Seminar/Sopt38-Seminar/SceneDelegate.swift
+++ Sopt38-Seminar/Sopt38-Seminar/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
                 
         let window = UIWindow(windowScene: windowScene)
-        let navigationController = UINavigationController(rootViewController: LoginViewController_CustomView())
+        let navigationController = UINavigationController(rootViewController: ChatViewController())
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
         self.window = window
