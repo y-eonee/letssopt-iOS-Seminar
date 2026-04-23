9541624f2f179e698502a3453dafe74c4ad55067
yeonee <leenayeon@sookmyung.ac.kr>
2026-04-23 17:27:47 +0900

    [4주차] 유저엔티티

---
+++ Sopt38-Seminar/Sopt38-Seminar/Week4/Entity/UserInfoEntity.swift
@@ -0,0 +1,17 @@
+//
+//  UserInfoEntity.swift
+//  Sopt38-Seminar
+//
+//  Created by 이나연 on 4/21/26.
+//
+
+import Foundation
+
+struct UserInfoEntity {
+    let id: Int
+    let loginID: String
+    let name: String
+    let email: String
+    let age: Int
+    let part: String
+}
