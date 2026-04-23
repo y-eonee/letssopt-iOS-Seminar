6f9cdaf7c7de1b4c6364c4ac5a52c80bf2b91c51
yeonee <leenayeon@sookmyung.ac.kr>
2026-04-23 17:28:22 +0900

    [4주차] Alert 익스텐션

---
+++ Sopt38-Seminar/Sopt38-Seminar/Week4/Extension/UIAlertController+.swift
@@ -0,0 +1,21 @@
+//
+//  UIAlertController+.swift
+//  Sopt38-Seminar
+//
+//  Created by 이나연 on 4/23/26.
+//
+
+import UIKit
+
+extension UIAlertController {
+    func showAlert(title: String, subMessage: String) {
+        let alert = UIAlertController(
+            title: title,
+            message : subMessage,
+            preferredStyle: .alert
+        )
+        
+        let okAction = UIAlertAction(title: "확인", style: .default)
+        alert.addAction(okAction)
+    }
+}
