ad4fbc7755873915945f748ad507f1e823e00f7b
yeonee <leenayeon@sookmyung.ac.kr>
2026-05-13 01:31:55 +0900

    [6주차] SwiftUI 세팅

---
+++ SOPT-38-SwiftUI/SOPT-38-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SOPT-38-SwiftUI
+//
+//  Created by 이나연 on 5/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SOPT-38-SwiftUI/SOPT-38-SwiftUI/SOPT_38_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOPT_38_SwiftUIApp.swift
+//  SOPT-38-SwiftUI
+//
+//  Created by 이나연 on 5/13/26.
+//
+
+import SwiftUI
+
+@main
+struct SOPT_38_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
